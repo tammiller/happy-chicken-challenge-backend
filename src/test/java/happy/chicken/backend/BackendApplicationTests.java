537d2d4e84d504ff72186dad77fc60b9b9ccd168
Emerald Fallarme <effallarme@gmail.com>
2023-10-15 10:30:59 -0400

    Initialize application run

---
+++ src/test/java/happy/chicken/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package happy.chicken.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
