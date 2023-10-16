cc84e7ab831167ff6c4b8576e0fdc49da6f523fe
Emerald Fallarme <effallarme@gmail.com>
2023-10-15 20:39:29 -0400

    Service for managing user data

---
+++ src/main/java/happy/chicken/backend/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package happy.chicken.backend.service;
+
+import happy.chicken.backend.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public void createUserAccount(User user) {
+
+    }
+}
