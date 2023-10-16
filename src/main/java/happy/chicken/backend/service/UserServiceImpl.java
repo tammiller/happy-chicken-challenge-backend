d6b60dfdf35caa50801425b862e7669e14d0e699
Emerald Fallarme <effallarme@gmail.com>
2023-10-15 20:45:00 -0400

    Placeholder for saving user

--- src/main/java/happy/chicken/backend/service/UserServiceImpl.java
+++ src/main/java/happy/chicken/backend/service/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package happy.chicken.backend.service;
 
 import happy.chicken.backend.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
     @Override
     public void createUserAccount(User user) {
-
+        log.info("saving user {}", user);
     }
 }
