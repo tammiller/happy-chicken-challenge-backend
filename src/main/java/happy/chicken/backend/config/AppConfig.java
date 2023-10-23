f5d42b90e6d51224fe76720af7de56e90ac5e1a2
Emerald Fallarme <effallarme@gmail.com>
2023-10-22 22:30:52 -0400

    Annotate nonnull parameter

--- src/main/java/happy/chicken/backend/config/AppConfig.java
+++ src/main/java/happy/chicken/backend/config/AppConfig.java
@@ -1,5 +1,6 @@
 package happy.chicken.backend.config;
 
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,7 +13,7 @@ public class AppConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 //TODO: For security, filter path allowed for CORS
                 registry.addMapping("/**").allowedOrigins("*");
             }
